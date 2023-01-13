497e1d5fab0453deb4985d7a4a9a4a53d5f6d285
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-01-12 21:37:02 -0600

    requisito de armazenamento realizado, conexao com o db feita.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
+dotenv.config()
+
+let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+mongoClient.connect()
+    .then(() => {
+        db = mongoClient.db();
+    })
+    .catch(() => {
+        console.log('fiz algo de errado na conexão com o db')
+    })
 
 const app = express();
 app.use(express.json());
@@ -14,5 +28,6 @@ app.post('', (req, res) => {
         
 });
 
+//LEMBRETE: alterar a porta do servidor para 5000 antes de enviar o projeto
 const PORT = 5003;
 app.listen (PORT, () => console.log(`servidor rodando na paz do Senhor na porta ${PORT}`))
