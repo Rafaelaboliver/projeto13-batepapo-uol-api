269ab7ab03496864cc205cf9798bb18ef045250e
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-01-13 14:14:29 -0600

    requisito POST (/participants) criado e a lista de usuários cadastrados está sendo recebida por meio da requisiçao GET(/participants) de acordo como solicitado nos requisitos.

--- src/app.js
+++ src/app.js
@@ -8,24 +8,46 @@ dotenv.config()
 let db;
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
-mongoClient.connect()
-    .then(() => {
-        db = mongoClient.db();
-    })
-    .catch(() => {
-        console.log('fiz algo de errado na conexão com o db')
-    })
+try {
+    await mongoClient.connect()
+    db = mongoClient.db();
+} catch (error) {
+    console.log('Erro ao conectar com o servidor!')
+}
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('', (req, res) => {
+
+//requisito: POST('participants')
+app.post('/participants', async (req, res) => {
+    const { name, lastStatus} = req.body
+
+    try {
+        const userCreated = await db.collection('participants').findOne({name});
+        if (userCreated) return res.status(409).send('Usuário já cadastrado!');
+
+        await db.collection('participants').insertOne({name, lastStatus: Date.now()});
+        res.status(201).send('Cadastro realizado com sucesso!');
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send('Erro no servidor');
+    };
 
 });
 
-app.post('', (req, res) => {
-        
+//requisito: GET('participants')
+app.get('/participants', async (req, res) => {
+     
+     try {
+        const info = await db.collection('participants').find().toArray();
+        return res.send(info);
+
+     } catch (error) {
+        res.status(500).send('Erro no servidor!')
+     }
 });
 
 //LEMBRETE: alterar a porta do servidor para 5000 antes de enviar o projeto
