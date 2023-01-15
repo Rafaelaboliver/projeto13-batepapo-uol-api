bc412b26e72165229d80c26884324c775291f46b
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-01-14 18:27:10 -0600

    requisito implementado: POST(/status) e a remoçao de usuários inativos por mais de 10 segundos.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import joi from 'joi';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId  } from 'mongodb';
 import dayjs from 'dayjs';
 
 dotenv.config()
@@ -38,11 +38,12 @@ app.post('/participants', async (req, res) => {
     }
     
     try {
-        const userCreated = await db.collection('participants').findOne({name: user.name});
+        const userCreated = await db.collection('participants').findOne({name: user.name.toLowerCase()});
         if (userCreated) return res.status(409).send('Usuário já cadastrado!');
 
-        await db.collection('participants').insertOne({name: user.name, lastStatus: Date.now()});
-        res.status(201).send('Cadastro realizado com sucesso!');
+        await db.collection('participants').insertOne({name: user.name.toLowerCase(), lastStatus: Date.now()});
+        await db.collection('messages').insertOne({from: user.name.toLowerCase(), to: 'Todos', text: 'entra na sala...', type: 'status', time:dayjs(Date.now()).format('HH:mm:ss') });
+        res.status(201).send('Ingresso realizado com sucesso!');
 
     } catch (error) {
         console.log(error);
@@ -67,10 +68,12 @@ app.get('/participants', async (req, res) => {
 //requisito: POST('messages')
 app.post('/messages', async (req, res) => {
     const data = req.body
+    const from = req.headers.user;
+
     const dataSchema = joi.object({
         to: joi.string().empty().required(),
         text: joi.string().empty().required(),
-        type: joi.required()
+        type: joi.string().valid('message', 'private_message').required()
     });
 
     const validation = dataSchema.validate(data, {abortEarly: false});
@@ -79,6 +82,11 @@ app.post('/messages', async (req, res) => {
             return err.message
         })
         return res.status(422).send(errors)
+    }
+
+    const user = await db.collection('participants').findOne({name: from});
+    if(!user) {
+        return res.status(401).send('UNAUTHORIZED');
     }
 
     try {
@@ -108,9 +116,40 @@ app.get('/messages', async (req, res) => {
 });
 
 //requisito: POST('status')
-app.post('/status', async (req, res) => {
+app.post('/status/:id', async (req, res) => {
 
-})
+const userUpdate = req.headers.user;
+const user = await db.collection('participants').findOne({name: userUpdate});
+if(!user) return res.status(404).send('Participante não encontrado');
+
+try {
+    await db.collection('participants').updateOne(
+        {name: userUpdate},
+        {$set: {lastStatus: Date.now()}}
+    );
+
+    res.status(200).send('Status atualizado!');
+
+} catch (error) {
+    console.log(error)
+    res.status(500).send('Erro no servidor!')
+}
+});
+
+//requisito: remoção automática dos usuários inativos
+function handlleDeleteUser() {
+
+    setInterval(async () => {
+        const participant = await db.collection('participants').find().toArray();
+        participant.map( async (item) => {
+            if(Date.now() - item.lastStatus > 10000) {
+                await db.collection('participants').deleteOne({name: item.name});
+                await db.collection('messages').insertOne({from: item.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: dayjs(Date.now()).format('HH:mm:ss')})
+            }
+        })
+}, 15000)}
+
+handlleDeleteUser();
 
 //LEMBRETE: alterar a porta do servidor para 5000 antes de enviar o projeto
 const PORT = 5003;
