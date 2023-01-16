e56af7391b10f35f6915aad5482e16b5d6c29956
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-01-15 18:14:53 -0600

    ajuste na porta

--- src/app.js
+++ src/app.js
@@ -163,5 +163,5 @@ function handlleDeleteUser() {
 handlleDeleteUser();
 
 //LEMBRETE: alterar a porta do servidor para 5000 antes de enviar o projeto
-const PORT = 5003;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`servidor rodando na paz do Senhor na porta ${PORT}`))
