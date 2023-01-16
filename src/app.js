daee3910dacdce9a3e9a601c26f2fc99c546e950
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-01-15 18:09:51 -0600

    ajustes no POST e GET /messages

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import joi from 'joi';
-import { MongoClient, ObjectId  } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import dayjs from 'dayjs';
 
 dotenv.config()
@@ -25,24 +25,25 @@ app.use(cors());
 //requisito: POST('participants')
 app.post('/participants', async (req, res) => {
     const user = req.body
+    
     const userSchema = joi.object({
         name: joi.string().empty().required()
     });
 
-    const validation = userSchema.validate(user, {abortEarly: false});
-    if(validation.error) {
+    const validation = userSchema.validate(user, { abortEarly: false });
+    if (validation.error) {
         const errors = validation.error.details.map((err) => {
             return err.message
         })
         return res.status(422).send(errors)
     }
-    
+
     try {
-        const userCreated = await db.collection('participants').findOne({name: user.name.toLowerCase()});
+        const userCreated = await db.collection('participants').findOne({ name: user.name.toLowerCase() });
         if (userCreated) return res.status(409).send('Usuário já cadastrado!');
 
-        await db.collection('participants').insertOne({name: user.name.toLowerCase(), lastStatus: Date.now()});
-        await db.collection('messages').insertOne({from: user.name.toLowerCase(), to: 'Todos', text: 'entra na sala...', type: 'status', time:dayjs(Date.now()).format('HH:mm:ss') });
+        await db.collection('participants').insertOne({ name: user.name.toLowerCase(), lastStatus: Date.now() });
+        await db.collection('messages').insertOne({ from: user.name.toLowerCase(), to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format('HH:mm:ss') });
         res.status(201).send('Ingresso realizado com sucesso!');
 
     } catch (error) {
@@ -54,15 +55,16 @@ app.post('/participants', async (req, res) => {
 
 //requisito: GET('participants')
 app.get('/participants', async (req, res) => {
-     
-     try {
-        
+
+    try {
+
         const info = await db.collection('participants').find().toArray();
+        console.log('INFO recebe=>', info)
         return res.send(info);
 
-     } catch (error) {
+    } catch (error) {
         res.status(500).send('Erro no servidor!')
-     }
+    }
 });
 
 //requisito: POST('messages')
@@ -76,64 +78,72 @@ app.post('/messages', async (req, res) => {
         type: joi.string().valid('message', 'private_message').required()
     });
 
-    const validation = dataSchema.validate(data, {abortEarly: false});
-    if(validation.error) {
+    const validation = dataSchema.validate(data, { abortEarly: false });
+    if (validation.error) {
         const errors = validation.error.details.map((err) => {
             return err.message
         })
         return res.status(422).send(errors)
     }
 
-    const user = await db.collection('participants').findOne({name: from});
-    if(!user) {
+    const user = await db.collection('participants').findOne({ name: from });
+    if (!user) {
         return res.status(401).send('UNAUTHORIZED');
     }
 
     try {
-        const model = await db.collection('messages').insertOne({to: data.to, text: data.text, type: data.type, time: dayjs(Date.now()).format('HH:mm:ss')});
+        const model = await db.collection('messages').insertOne({ from: from, to: data.to, text: data.text, type: data.type, time: dayjs(Date.now()).format('HH:mm:ss') });
         return res.status(201).send(model);
 
     } catch (error) {
         console.log(error);
         res.status(500).send('Erro no servidor!');
     };
-    
+
 
 });
 
 //requisito: GET('messages')
 app.get('/messages', async (req, res) => {
-     
+    const user = req.headers.user;
     try {
-        const {limit} = req.query;
-        const message = await db.collection('messages').find().toArray();
+        const { limit } = req.query;
+        const message = await db.collection('messages').find({
+            $or: [
+                { to: user },
+                { from: user },
+                { to: "Todos" },
+                { type: "message" },
+            ],
+        }).toArray();
         const display = (message.length - limit)
-        return res.send(message.slice(display));
+        return res.send(message.slice(display).reverse());
 
     } catch (error) {
         res.status(500).send('Erro no servidor!')
     }
+
 });
 
 //requisito: POST('status')
-app.post('/status/:id', async (req, res) => {
+app.post('/status', async (req, res) => {
 
-const userUpdate = req.headers.user;
-const user = await db.collection('participants').findOne({name: userUpdate});
-if(!user) return res.status(404).send('Participante não encontrado');
+    const userUpdate = req.headers.user;
+    const user = await db.collection('participants').findOne({ name: userUpdate });
+    if (!user) return res.status(404).send('Participante não encontrado');
 
-try {
-    await db.collection('participants').updateOne(
-        {name: userUpdate},
-        {$set: {lastStatus: Date.now()}}
-    );
+    try {
+        await db.collection('participants').updateOne(
+            { name: userUpdate },
+            { $set: { lastStatus: Date.now() } }
+        );
 
-    res.status(200).send('Status atualizado!');
+        res.status(200).send('Status atualizado!');
 
-} catch (error) {
-    console.log(error)
-    res.status(500).send('Erro no servidor!')
-}
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Erro no servidor!')
+    }
 });
 
 //requisito: remoção automática dos usuários inativos
@@ -141,13 +151,14 @@ function handlleDeleteUser() {
 
     setInterval(async () => {
         const participant = await db.collection('participants').find().toArray();
-        participant.map( async (item) => {
-            if(Date.now() - item.lastStatus > 10000) {
-                await db.collection('participants').deleteOne({name: item.name});
-                await db.collection('messages').insertOne({from: item.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: dayjs(Date.now()).format('HH:mm:ss')})
+        participant.map(async (item) => {
+            if (Date.now() - item.lastStatus > 10000) {
+                await db.collection('participants').deleteOne({ name: item.name });
+                await db.collection('messages').insertOne({ from: item.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: dayjs(Date.now()).format('HH:mm:ss') })
             }
         })
-}, 15000)}
+    }, 15000)
+}
 
 handlleDeleteUser();
 
